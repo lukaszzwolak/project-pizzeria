9830a004ec49e90ad03830285d65bae17021bdb8
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-02-24 22:19:11 +0100

    adding the first class, Product constructor

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,9 +3,9 @@
 {
   'use strict';
 
-const select = {
-  templateOf: {
-    menuProduct: "#template-menu-product",
+  const select = {
+    templateOf: {
+      menuProduct: "#template-menu-product",
     },
     containerOf: {
       menu: '#product-list',
@@ -52,14 +52,65 @@ const select = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+
+      console.log('new Product: ', thisProduct);
+    }
+
+    renderInMenu() {
+      const thisProduct = this;
+
+    }
+
+    initAccordion() {
+      const thisProduct = this;
+      console.log('Accordion initialized for product: ', thisProduct.id);
+    }
+  }
+
   const app = {
-    init: function(){
+    initMenu: function () {
+      const thisApp = this;
+      console.log('thisApp.data: ', thisApp.data);
+
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+
+      //generowanie html
+      const generatedHTML = templates.menuProduct(thisApp.data);
+      //tworzenie elementu DOM
+      const element = utils.createDOMFromHTML(generatedHTML);
+      //szukanie kontener menu
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      //dodawanie stworzony element na strone
+      menuContainer.appendChild(element);
+    },
+
+    init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
