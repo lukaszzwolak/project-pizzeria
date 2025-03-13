780562d41997d3a3495575071b54111b2ca9b70d
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-03-13 19:53:32 +0100

    Integrate DatePicker and HourPicker widgets in Booking class

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -20,8 +20,8 @@ class Booking {
         thisBooking.dom.wrapper.innerHTML = generateHTML;
         thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
         thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
-
-
+        thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+        thisBooking.dom.hoursPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
     }
 
     initWidgets() {
@@ -29,6 +29,8 @@ class Booking {
 
         thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
         thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+        thisBooking.datePicker = new AmountWidget(thisBooking.dom.datePicker);
+        thisBooking.hoursPicker = new AmountWidget(thisBooking.dom.hoursPicker);
         thisBooking.dom.peopleAmount.addEventListener('click', function () { });
         thisBooking.dom.hoursAmount.addEventListener('click', function () { });
     }
